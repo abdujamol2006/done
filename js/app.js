aaf004755f1c761167183d587b958afcc09f86bf
Abdujamol <hamdamjonovabdujamol@gmail.com>
2024-04-21 23:47:43 +0500

    sd

--- js/app.js
+++ js/app.js
@@ -1,4 +1,5 @@
 const cocktailscenter = document.querySelector(".cocktails-center");
+const cocktailsection = document.querySelector("cocktail-section");
 const loader = document.querySelector(".loader");
 loader.classList.remove("hidden");
 
@@ -11,7 +12,7 @@ function createDrinks(data) {
               <h3>${item.strDrink}</h3>
               <h4>${item.strGlass}</h4>
               <p>${item.strAlcoholic}</p>
-              <a class="btn btn-primary btn-details" href="/coctail/${item.idDrink}"
+              <a  id="details"class="btn btn-primary btn-details" href="/coctail/${item.idDrink}"
                 >details</a
               >`;
 
@@ -72,18 +73,38 @@ fetch("https:www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${id}")
     drink.classList.add("drink");
   }); const details = document.createElement("details");
     details.classList.add("information");
-    details.innerHTML = `<img src=${item.strDrinkThumb} alt='${item.strDrink}' />
+function createUi(data, dataType) {
+  cocktailsection.innerHTML = "";
+  data.forEach((item) => {
+    const details = document.createElement("details");
+    details.classList.add("information");
+    switch (dataType) {
+      case "data":
+        details.innerHTML = `<img src=${item.strDrinkThumb} alt='${item.strDrink}' />
           <div class="drink-info">
-            <p><span class="drink-data">${item.strName}</span>${item.strName} </p>
-            <p><span class="drink-data">${item.strCategory} </span> ${item.strCategory}</p>
-            <p><span class="drink-data">${item.strInfo} </span> ${item.strInfo}</p>
-            <p><span class="drink-data">${item.strGlass}</span> ${item.strGlass}</p>
+            <p><span class="drink-data">name :</span>${item.strName} </p>
+            <p><span class="drink-data">category :</span> ${item.strCategory}</p>
+            <p><span class="drink-data">info :</span> ${item.strInfo}</p>
+            <p><span class="drink-data">glass :</span> ${item.strGlass}</p>
             <p>
-              <span class="drink-data">${item.strInstructons} </span> ${item.strInstructons} 
+              <span class="drink-data">instructons : </span> ${item.strInstructons} 
             </p>
             <p>
-              <span class="drink-data">${item.striIngredients} </span><span>${item.striIngredients} </span
+              <span class="drink-data">ingredients : </span><span>${item.striIngredients} </span
               ><span> ${item.striIngredients} </span><span> ${item.striIngredients} </span
               ><span> ${item.striIngredients} </span>
             </p>
-          </div>`;*/
+          </div>`;
+        break;
+    }
+  });
+}
+details.addEventListener("click", (e) => {
+  getData("data");
+  function getData(dataType) {
+    fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=`)
+      .then((res) => res.json())
+      .then((data) => createDrinks(data, dataType));
+    .catch((err) => alert("Eror"));
+  }
+});*/
